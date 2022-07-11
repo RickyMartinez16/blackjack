cbdb2f6dacda268a91243e7ea3fdba68ab92fe7a
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-10 22:45:51 -0400

    16

---
+++ index.js
@@ -0,0 +1,33 @@
+function generateRandomInt(min,max){
+    return Math.floor((Math.random() * (max+1 -min)) +min);
+}
+
+let firstCard = generateRandomInt(2, 11);
+let secondCard = generateRandomInt(2, 11);
+
+let sum = firstCard + secondCard;
+
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el")
+
+function start(){
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;   
+}
